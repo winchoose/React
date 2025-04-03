fdd4e8258ce0a2a9ac9e934a26073a243a1ce624
winchoose <tmdxor741@naver.com>
2025-04-03 19:24:44 +0900

    [feat] async&await 가독성 있게 수정

--- fetch/practice.js/api.js
+++ fetch/practice.js/api.js
@@ -1,4 +1,4 @@
-async function mokData() {
+/*async function mokData() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
                 fetch("https://jsonplaceholder.typicode.com/users")
@@ -7,10 +7,17 @@ async function mokData() {
                 .catch(error => reject(error))
         }, 1500)
     })
+}*/
+//더 가독성 있게 바꾸면
+async function mockData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users'); 
+    const data = await response.json(); 
+    return data; 
 }
 
+
 async function printData() {
-    const data = await mokData();
+    const data = await mockData();
     console.log(data);
 }
 printData();
