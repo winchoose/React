99fe64fa2cea4d06db0cac4b4e77cda55cf87d47
winchoose <tmdxor741@naver.com>
2025-06-27 00:50:42 +0900

    [feat] 폰트, 이미지, 레이아웃 설정

--- inflearn/section08/section08/src/App.jsx
+++ inflearn/section08/section08/src/App.jsx
@@ -2,18 +2,41 @@ import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
 import Notfound from './pages/Notfound'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import './App.css'
 
+import { getEmotionImage } from './util/get-emotion-image'
+
+
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  }
 
   return (
+    <>
+    <div>
+      <img src={getEmotionImage(1)}/>
+      <img src={getEmotionImage(2)}/>
+      <img src={getEmotionImage(3)}/>
+      <img src={getEmotionImage(4)}/>
+      <img src={getEmotionImage(5)}/>
+    </div>
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/new"}>New</Link>
+      <Link to={"/diary"}>Diary</Link>
+    </div>
+    <button onClick={onClickButton}>New 페이지로 이동</button>
   <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/new' element={<New/>}/>
-    <Route path='/diary' element={<Diary/>}/>
+    <Route path='/diary/:id' element={<Diary/>}/>
     <Route path='*' element={<Notfound/>}/>
   </Routes>
+  </>
   )
 }
 
